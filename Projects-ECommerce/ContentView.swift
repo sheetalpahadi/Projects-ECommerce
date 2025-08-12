a4d62bef53d960bf23736f4c21f59c009c88847d
Sheetal Pahadi <sheetal992009@gmail.com>
2025-08-12 21:24:59 +0530

    Initial Commit

---
+++ Projects-ECommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projects-ECommerce
+//
+//  Created by Admin on 12/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects-ECommerce/Projects_ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projects_ECommerceApp.swift
+//  Projects-ECommerce
+//
+//  Created by Admin on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Projects_ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
